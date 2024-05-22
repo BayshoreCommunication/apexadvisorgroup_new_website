50926ff3e23a2d7540da1354985d0f3974a05226
abukawsar47 <abukawsar47ak@gmail.com>
2024-05-22 16:54:19 +0600

    contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,8 +2,7 @@ import PageHeroSection from "@/components/shared/PageHeroSection";
 import React from "react";
 import ContactPageImg from "../../public/image/contact.jpg";
 import MapSection from "@/components/contact/MapSection";
-import ContactSection from "@/components/contact-us/ContactSection";
-// import ContactSection from "@/components/contact/ContactSection";
+import ContactSection from "@/components/contact/ContactSection";
 
 const page = () => {
   return (
@@ -18,7 +17,6 @@ const page = () => {
         width={1000}
         height={657}
       />
-      <ContactSection />
       <ContactSection />
       <MapSection />
     </div>
--- components/contact/ContactSection.jsx
+++ components/contact/ContactSection.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import SectionLayout from "../shared/SectionLayout";
+import SecondaryButton from "../shared/SecondaryButton";
 
 const ContactSection = () => {
   return (
     <SectionLayout>
-      <div className="flex flex-col lg:flex-row items-center">
+      <div className="flex flex-col lg:flex-row gap-6 items-center">
         {/* contact-address */}
-        <div class=" flex-1">
+        <div class="w-full flex-1">
           <div className="mb-5">
             <h3 className="text-2xl font-semibold mb-2">Address:</h3>
             <p>1211 TECH BLVD, SUITE 120, TAMPA, FL 33619</p>
@@ -26,47 +27,72 @@ const ContactSection = () => {
           </div>
         </div>
         {/* contact-form  */}
-        <div class="flex-1">
+        <div class="w-full flex-1">
           <form>
-            <div class="contact-form-name-container">
-              <div class="contact-form-name">
-                <p>First Name *</p>
+            <div class="flex flex-col lg:flex-row w-full gap-4 mb-4">
+              <div class="w-full">
+                <p className="text-lg font-semibold mb-2">First Name *</p>
                 <input
+                  className="p-2 border-1 border-[#0b7788] w-full"
                   type="text"
                   placeholder="Enter your first name."
                   value=""
                 />
               </div>
-              <div class="contact-form-name">
-                <p>Last Name *</p>
+              <div class="w-full">
+                <p className="text-lg font-semibold mb-2">Last Name *</p>
                 <input
+                  className="p-2 border-1 border-[#0b7788] w-full"
                   type="text"
                   placeholder="Enter your last name."
                   value=""
                 />
               </div>
             </div>
-            <p>Email *</p>
-            <input type="text" placeholder="Enter your email." value="" />
-            <p>Phone *</p>
-            <input
-              type="text"
-              placeholder="Enter your phone number."
-              value=""
+            <div className="mb-4">
+              <p className="text-lg font-semibold mb-2">Email *</p>
+              <input
+                className="p-2 border-1 border-[#0b7788] w-full"
+                type="text"
+                placeholder="Enter your email."
+                value=""
+              />
+            </div>
+            <div className="mb-4">
+              <p className="text-lg font-semibold mb-2">Phone *</p>
+              <input
+                className="p-2 border-1 border-[#0b7788] w-full"
+                type="text"
+                placeholder="Enter your phone number."
+                value=""
+              />
+            </div>
+            <div className="mb-4">
+              <p className="text-lg font-semibold mb-2">
+                Your Organization (if any)
+              </p>
+              <input
+                className="p-2 border-1 border-[#0b7788] w-full"
+                type="text"
+                placeholder="Enter organization name."
+                value=""
+              />
+            </div>
+            <div className="mb-4">
+              <p className="text-lg font-semibold mb-2">Message *</p>
+              <textarea
+                className="p-2 border-1 border-[#0b7788] w-full"
+                type="text"
+                placeholder="Enter message."
+                rows="7"
+              ></textarea>
+            </div>
+            <SecondaryButton
+              title={"Send"}
+              link={"/contact"}
+              style={"bg-[#0E758B] text-white p-2"}
+              radius={"sm"}
             />
-            <p>Your Organization (if any)</p>
-            <input
-              type="text"
-              placeholder="Enter organization name."
-              value=""
-            />
-            <p>Message *</p>
-            <textarea
-              type="text"
-              placeholder="Enter message."
-              rows="7"
-            ></textarea>
-            <button type="submit">Send</button>
           </form>
         </div>
       </div>
