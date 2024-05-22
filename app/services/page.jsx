9f66974855477cb00adae8092422f83c66302cbb
arsahak <arsahak99@gmail.com>
2024-05-22 17:31:27 +0600

    update

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import Services from '@/components/services/Services';
+import Consultation from '@/components/shared/Consultation';
+import PageHeroSection from '@/components/shared/PageHeroSection';
 
 const page = () => {
   return (
     <div>
       <PageHeroSection
-        heading={"Services"}
-        subheading={"By Apex Advisor Group Inc"}
+        heading={'Services'}
+        subheading={'By Apex Advisor Group Inc'}
         image={ServicePage}
         width={1000}
         height={657}
--- components/home/AboutSection.jsx
+++ components/home/AboutSection.jsx
@@ -1,34 +1,23 @@
-"use client";
-import Image from "next/image";
-import React from "react";
-import SectionLayout from "../shared/SectionLayout";
-import CardMotion from "../motion/CardMotion";
-import ImageMotion from "../motion/ImageMotion";
-import MotionEffect from "../motion/MotionEffect";
-import { motion } from "framer-motion";
-import { Mulish, Bitter } from "next/font/google";
-import BlackButton from "../shared/BlackButton";
-import RevealMotion from "../motion/RevealMotion";
-import ScondayButton from "../shared/ScondayButton";
-import { Suspense } from "react";
-import VideoPlayer from "../shared/Video/VideoPlayer";
+'use client';
+import React from 'react';
+import SectionLayout from '../shared/SectionLayout';
 
 const AboutSection = () => {
   return (
-    <SectionLayout bg="bg-slate-50 ">
-      <div className="space-y-14">
-        <div className="flex items-center justify-center">
-          <div className="">
+    <SectionLayout bg='bg-slate-50 '>
+      <div className='space-y-14'>
+        <div className='flex items-center justify-center'>
+          <div className=''>
             <h2
               className={`text-stone-950 font-bold text-5xl mt-5 mb-4 text-center `}
             >
               Apex Advisor Group Inc
             </h2>
-            <h4 className="mb-4 text-lg text-stone-950 font-bold text-center">
+            <h4 className='mb-4 text-lg text-stone-950 font-bold text-center'>
               Accounting Firm In Tampa, Florida
             </h4>
 
-            <p className="mb-4 text-lg text-stone-950 text-center">
+            <p className='mb-4 text-lg text-stone-950 text-center'>
               Bookkeeping, Payroll, Tax Preparation, Credit Repair & Counseling
               We here at Apex Advisor Group Inc are a team of highly qualified
               professionals that have over 40+ years of combined experience in
@@ -38,15 +27,15 @@ const AboutSection = () => {
               Advisor Group Inc is the right choice for you, your family and
               your business.
             </p>
-            <p className="mb-4 text-lg text-stone-950 text-center">
-              Our Tax Accountants serve clients across{" "}
+            <p className='mb-4 text-lg text-stone-950 text-center'>
+              Our Tax Accountants serve clients across{' '}
               <strong>
                 Tampa, Brandon, Riverview, Sun City, Ruskin, Plant City, Wesley
-                Chapel, Lithia{" "}
+                Chapel, Lithia{' '}
               </strong>
               and the surrounding areas.
             </p>
-            <p className="mb-4 text-lg text-stone-950 text-center">
+            <p className='mb-4 text-lg text-stone-950 text-center'>
               <strong>Contact Apex Advisor Group Inc</strong> - Tax Preparation
               & Accounting Firm in Tampa, Florida.
             </p>
